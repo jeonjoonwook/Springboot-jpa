78f66f88a19e8aa7bc0158d389e76828035d5c6b
jeonjoonwook <wook6139@naver.com>
2021-06-30 21:52:01 +0900

    domain 생성

--- src/main/java/jpabook/jpashop/domain/Member2.java
+++ src/main/java/jpabook/jpashop/domain/Member2.java
@@ -1,18 +1,29 @@
 package jpabook.jpashop.domain;
 
+
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
 public class Member2 {
 
     //@ID는 primary key @GeneratedValue는 주키를 어떻게 생성할지
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
+    @Column(name = "member2_id")
     private Long id;
-    private String username;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member2")
+    private List<Order> orders = new ArrayList();
+
 }
